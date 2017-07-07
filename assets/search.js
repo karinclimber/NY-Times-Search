001a1fc70fb7eddcff327a032ac990ec94feef85
Alexander Vincent <AlexV9582@yahoo.com>
2017-07-06 21:25:37 -0400

    Branch push

--- assets/search.js
+++ assets/search.js
@@ -6,17 +6,16 @@ var sort
 
 
 var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-queryurl += '?' + $.param({
+queryUrl += '?' + $.param({
 	'api-key': "48a4be7acc7344319bd90c8d78a89c08",
 	'q': q,
 	'begin_date': begin_date,
 	'end_date': end_date,
-	'sort': sort
-
-}
+});
 $.ajax({
-	url: queryUrl
-	method: "GET"
+	url: queryUrl,
+	method: "GET",
 }).done(function(response) {
-	console.log(response)
-})
+	console.log(response);
+});
+console.log("test")
